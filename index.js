b166915a9eaf624ce3765fdaa0a57306592edb34
daudi <david.otieno58@gmail.com>
2023-04-02 17:59:57 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+console.log(multiply);
+
+
+let random = Math.ceil(Math.random() * 5); 
+
+console.log(random);
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+console.log(mod);
+
+let setOfNumbers = [7, 11, 20, 15, 6];
+let max = Math.max(...setOfNumbers);
+console.log(max); 
